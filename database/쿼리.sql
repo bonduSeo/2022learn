152e69301567e32a3d0c076508d3e1e93b9f81c6
bondu_green <bondu.seo@gmail.com>
2022-03-28 15:49:18 +0900

    db

---
+++ database/쿼리.sql
@@ -0,0 +1,128 @@
+CREATE DATABASE test1;
+DROP DATABASE test1;
+
+CREATE TABLE memberTBL (
+	memberID CHAR(8) NOT NULL PRIMARY KEY,
+	memberName CHAR(5) NOT NULL,
+	memberAddress CHAR(20) null
+);
+/* prymary key
+unique(중복된 값이 안들어가는), not null, index*/
+
+-- 주석방법. --하고 하고 한칸 띄어야함. 싱글라인 주석
+/* 요거도 됨 멀티라인 주석 */ 
+
+DROP TABLE membertbl;
+
+CREATE TABLE productTBL(
+	productName CHAR(4) PRIMARY KEY,
+	cost INT NOT NULL,
+	MAKEDATE DATE,
+	company CHAR(5),
+	amount INT NOT null
+);
+
+-- int:정수, 실수(소수점이 있으면)
+
+
+-- CRUD, DML (데이터 조작어)
+-- Create ( insert문)
+/*
+INSERT INTO  테이블명
+(값을 넗고싶은 컬럼명)
+VALUES 
+(값들)
+띄어지기만하면 띄어쓰기 줄바꿈 상관없음
+*/
+INSERT INTO membertbl
+(memberid, membername, memberaddress)
+VALUES
+(1, '홍길동','서울시');
+
+INSERT INTO membertbl
+(memberid, membername, memberaddress)
+VALUES
+(2, '홍길동동동','서울울시');
+
+INSERT INTO membertbl
+(memberid, membername)
+VALUES
+(3, '홍길동');
+
+INSERT INTO membertbl
+(membername, memberid)
+VALUES
+('홍길동',3);
+-- 이렇게도 쌉가능
+
+
+-- membername 은 not null 이기때문에 에러가 발생
+INSERT INTO membertbl
+(memberid)
+VALUES
+(4);
+
+INSERT INTO membertbl
+(memberid, membername)
+VALUES
+(5, '신사임당'),
+(6, '유관순'),
+(7, '나이팅게일');
+
+INSERT INTO membertbl
+VALUES
+(8, '신사임당', '부산시');
+-- 이렇게 되긴하는데 에러터질수도있고 안쓰는게 낫다. (DB에서 컬럼순서가 바뀔수도있기에)
+
+
+-- Read (select문)
+/*
+select 보고싶은 컬럼명들 from 테이블명
+[옵션]
+[join 테이블 연결]
+[where 보고싶은 조건]
+[group by 그룹으로 묶은 조건]
+[having 그룹으로 묶는 조건에서의 조건]
+[order by 레코드 순서 지정]
+[limit 보고싶은 레코드 수]
+*/
+SELECT memberid, membername, memberaddress
+FROM membertbl;
+
+SELECT membername, memberid
+FROM membertbl;
+
+SELECT * FROM membertbl;
+
+SELECT * FROM membertbl
+WHERE memberid = '1';
+
+SELECT * FROM membertbl
+WHERE memberaddress = '서울시';
+
+SELECT * from membertbl
+WHERE memberaddress != '서울시';
+-- 서울시가 아닌 값이 있는 
+
+SELECT * FROM membertbl
+WHERE memberaddress IS NULL;
+-- null 인 항목
+
+SELECT * FROM membertbl
+WHERE memberaddress != '서울시'
+or memberaddress IS NULL;
+-- 난 둘돠 
+
+SELECT * FROM membertbl
+	WHERE memberaddress ='부산시'
+		AND membername = '신사임당';
+		
+
+
+-- Update (update문)
+/*
+	update 테이블명
+	set 수정하고 싶은 컬럼명 = 변경하고 싶은 값
+	where 레코드 선택
+*/
+-- Delete (delete문)
