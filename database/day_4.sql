c33566bfceb1233716ac300be868ecfaf093ca43
bondu_green <bondu.seo@gmail.com>
2022-03-31 12:13:17 +0900

    dd

--- database/day_4.sql
+++ database/day_4.sql
@@ -1,1 +1,13 @@
 -- day 4
+
+CREATE TABLE t_member_hobbit (
+	user_no INT,
+	hobbit_no INT,
+	PRIMARY KEY (user_no, hobbit_no),
+	FOREIGN KEY (user_no) REFERENCES membertbl (memberid),
+	FOREIGN KEY (hobbit_no) REFERENCES t_hobbit_2 (hobbit_id)
+);
+
+SELECT * FROM t_member_hobbit;
+
+DESC t_member_hobbit;
